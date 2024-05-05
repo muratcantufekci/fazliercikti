4e53c136e27b854b9f9f37d6a4819ef2c25bf266
muratcan.tufekci <muratcan.tufekci@demirorenmedya.com>
2024-05-05 23:45:18 +0300

    swiper initialized

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,2 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    // loop: true,
+    navigation: {
+      nextEl: ".activity-button-next",
+      prevEl: ".activity-button-prev",
+    },
+  });
 });
