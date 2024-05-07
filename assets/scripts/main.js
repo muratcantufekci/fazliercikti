71d11e5061d2f2641b154f2e14abc99e36425d08
muratcan.tufekci <muratcan.tufekci@demirorenmedya.com>
2024-05-07 13:56:28 +0300

    homepage desktop done

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const swiper = new Swiper(".swiper", {
+  const activitySwiper = new Swiper(".activity-swiper", {
     slidesPerView: 3,
-    spaceBetween: 20,
-    // loop: true,
+    spaceBetween: 10,
     navigation: {
       nextEl: ".activity-button-next",
       prevEl: ".activity-button-prev",
+    },
+  });
+
+  const articlesSwiper = new Swiper(".articles-swiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".articles-button-next",
+      prevEl: ".articles-button-prev",
     },
   });
 });
