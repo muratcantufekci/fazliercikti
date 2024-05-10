47e8f79b681d563590e98c1d8a07fd35b9c5a8bd
muratcan.tufekci <muratcan.tufekci@demirorenmedya.com>
2024-05-10 20:40:25 +0300

    homepage responsive and hamburger menu done

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,28 @@
+const handleHamburgerMenu = () => {
+  const openHamburger = document.getElementById('js-open-hamburger')
+  const closeHamburger = document.getElementById('js-close-hamburger')
+  const hamburgerMenu = document.querySelector('.o-header__hamburger')
+
+  openHamburger.addEventListener('click', () => {
+    hamburgerMenu.classList.add('active')
+    document.body.style.overflow = 'hidden'
+  })
+
+  closeHamburger.addEventListener('click', () => {
+    hamburgerMenu.classList.remove('active')
+    document.body.style.overflow = 'auto'
+  })
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const activitySwiper = new Swiper(".activity-swiper", {
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 10,
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+      }
+    },
     navigation: {
       nextEl: ".activity-button-next",
       prevEl: ".activity-button-prev",
@@ -9,11 +30,21 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   const articlesSwiper = new Swiper(".articles-swiper", {
-    slidesPerView: 4,
+    slidesPerView: 1,
     spaceBetween: 20,
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      }
+    },
     navigation: {
       nextEl: ".articles-button-next",
       prevEl: ".articles-button-prev",
     },
   });
+
+  handleHamburgerMenu();
 });
